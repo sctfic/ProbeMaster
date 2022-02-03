cd8031d0c8e82de5a80ac223c00c550f40d177ec
LOPEZ Alban <alban.lopez+ProbeMaster@gmail.com>
2022-02-03 23:59:43 +0000

    test(structure):tentative de sous fichier mais je ne comprend pas le scope des variables et lasynchrone

--- router.js
+++ router.js
@@ -4,6 +4,7 @@ const network = require('network');
 // const fs = require('fs');
 const ping = require('ping');
 const axios = require('axios');
+const interfacesJS = require('./network/interfaces');
 
 router
 	.get("/", (requestuest, response) => {
@@ -17,67 +18,62 @@ router
 
 	.get('/interfaces/', function (request, response) {
 		network.get_interfaces_list(function(err, list) {
-			console.log(err || list);
-			response.send('<pre>'+JSON.stringify(list)+'</pre>');
-		})
+			response.json (list);
+		});
 	})
 
-	.get('/ping/', async function (request, response) {
-		const hosts = ['imp','192.168.1.1','google.com', 'yahoo.com'];
-
-		for(let host of hosts){
-			let res = await ping.promise.probe(host);
-			console.log(res);
-		}
-		response.json(res)
-
-
-
-		// const pings = [];
-		// hosts.forEach(function (host) {
-		// 	// WARNING: -i 2 argument may not work in other platform like window
-		// 	ping.promise.probe(host, {
-		// 			timeout: false,
-		// 			// Below extra arguments may not work in platforms other than linux
-		// 			extra: ['-i', '2'],
-		// 	})
-		// 	.then(function (response) {
-		// 			console.log(response);
-		// 			pings.push(2);
-		// 	})
-		// 	.done(
-		// 		function (response) {
-		// 			console.log(response);
-		// 		}
-		// 	);
-		// 	pings.push(3);
-		// 	response.json(pings)
-		// });
-	})
-
-	.get('/nasa/', function (request, response) {
-		axios.all([
-				axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2017-08-03'),
-				axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2017-08-02')
-			]).then(axios.spread((response1, response2) => {
-				console.log(response1.data.url);
-				console.log(response2.data.url);
-				response.json(response2.data)
-				// response.send('<pre>'+JSON.stringify(response2.data)+'</pre>');
-			})).catch(error => {
+    .get("/interfaces/:name",(request,response)=>{
+        // interfacesJS.getInterface(request.params.name)
+		axios.get('http://localhost:3001/interfaces/')
+			.then(function (resp) {
+				// handle success
+				// console.log(resp.data)
+				const interface = resp.data.find(x => x.name === request.params.name)
+				response.json(interface)
+			})
+			.catch(function (error) {
+				// handle error
 				console.log(error);
+				response.status(404);
+				response.json({
+					error: "API not found"
+				})
+			})
+			.then(function () {
+				// always executed
 			});
-		})
 
-	.get('/data/', (request,response) => {
-		const data = [
-			{"name":"test de data!"},
-			{"name": "eth0","ip_address": "192.168.1.100","mac_address": "b8:27:eb:dc:93:51","gateway_ip": "192.168.1.1","netmask": "255.255.255.128","type": "Wired"},
-			{"name": "wlan0","ip_address": "192.168.1.200","mac_address": "b8:27:eb:89:c6:04","netmask": "255.255.255.128","type": "Wireless"}
-		]
-		response.json(data)
 	})
 
+	.get('/ping/:host', async function (request, response) {
+
+		let res = await ping.promise.probe(request.params.host);
+		console.log(res);
+		response.json(res)
+	})
+	.get('/pings/', function (request, response) {
+
+		const hosts = ['imp', 'yahoo.com','192.168.1.1','google.com'];
+		hosts.forEach(function (host) {
+				// WARNING: -i 2 argument may not work in other platform like window
+			ping.promise.probe(host, {
+				timeout: false,
+				// Below extra arguments may not work in platforms other than linux
+				extra: ['-i', '2'],
+			})
+			.then(function (resp) {
+				console.log('display',resp);
+				return resp
+			})
+			.done(
+				// function (resp) {
+				// 	console.log(resp);
+				// }
+			)
+		});
+	})
+
+	
 	.use((request, response) => {
 		response.status(404);
 		response.json({
