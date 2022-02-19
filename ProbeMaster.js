4bd8fdfd2e31bc531b38e3899f27fbcedf7663b3
LOPEZ Alban <alban.lopez+ProbeMaster@gmail.com>
2022-02-19 10:02:45 +0000

    feat(UI):first console html

--- ProbeMaster.js
+++ ProbeMaster.js
@@ -1,27 +1,28 @@
+const port = process.env.PORT || 3001;
 const express = require('express');
+const app = express();
 const router = require('./router');
 
 // CORS permet de configurer comment des applications web définies sur un autre domaine peuvent accéder aux ressources de votre serveur. Ce mécanisme est appelé CORS pour Cross-Origin Resource Sharing, d’où le nom de ce package. Faire appel à ce package sans lui passer d’arguments permet d’autoriser tous les accès à votre ressource
-const cors = require('cors');
+// const cors = require('cors');
 
 // MORGAN permet de définir les informations que le serveur affiche dans la console à chaque fois qu’il reçoit une requête HTTP
 // const morgan = require('morgan');
 
 // permet de décomposer les requêtes HTTP POST, PATCH, etc. afin de pouvoir extraire les infoirmations tranmises dans des formulaires. Ces informations apparaissent dans le champ req.body
-const bodyParser = require("body-parser");
-
-const app = express();
-
-const port = process.env.PORT || 3001;
+// const bodyParser = require("body-parser");
 
 // app.use(morgan('combined')); 
-app.use(cors());
-app.use(bodyParser.json()); 
-app.use(bodyParser.urlencoded({
-        extended: true
-    }));
+// app.use(cors());
+// app.use(bodyParser.json()); 
+// app.use(bodyParser.urlencoded({
+//         extended: true
+//     }));
 
+// Defini les repertoire dont l'url n'est pas interprete par le ROUTER mais juste servie directement
+app.use(express.static(__dirname + '/statics'));
 
 // permet de définir les routes dans le fichier router.js
 app.use(router);
+
 app.listen(port, () => console.log('Server app listening on port ' + port));
--- router.js
+++ router.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const network = require('network');
+const path = require('path');
 // const fs = require('fs');
 const axios = require('axios');
 // const sshJS = require('./network/ssh');
 // const ssh2JS = require('./network/ssh2');
 const pingJS = require('./network/ping');
 const gpio = require('array-gpio');
+
 
 const relays = [gpio.out(7),gpio.out(15),gpio.out(31),gpio.out(37)]
 	relays[0]._index = {Relay:1, Pin:7, BCM:4,  wPi:7, query:null,succes:null, state:relays[0].state}
@@ -25,17 +27,20 @@ async function getAllPromises(Promises){
 }
 
 router
-	.get("/", (requestuest, response) => {
+	.get("/", (request, response) => {
+		response.sendFile(path.resolve(__dirname, 'UI/', 'index.html'));
+	})
+	.get("/ip", (request, response) => {
 		const data = {};
 		const ippublic = network.get_public_ip(function(err, ip) {
 			console.log('IP Publique : '+err || ip); // should return your public IP address
 			response.send('IP Publique = '+ip);
 		})
 	})
-	// .get("/ssh/", (requestuest, response) => {
+	// .get("/ssh/", (request, response) => {
 	// 	sshJS.getTunnel()
 	// })
-	// .get("/ssh2/", (requestuest, response) => {
+	// .get("/ssh2/", (request, response) => {
 	// 	ssh2JS.getTunnel()
 	// })
 	.get('/API/interface/', async function (request, response) {
@@ -48,29 +53,37 @@ router
 		// const list = await interfacesJS.getAllInterface()
         // console.log('>>>',list)
 	})
-	.get("/API/cron/", (requestuest, response) => {
+	.get("/API/cron/:time?", (request, response) => {
 		const CronJob = require('cron').CronJob;
-		const job = new CronJob('* * * * * *', function() {
+		let time = ''
+		if(request.params.time) {time = request.params.time}
+		let callbackCron = function (time) {
 			let x = (new Date()).getSeconds()%16
-			console.log('/API/relay/'+(((x >> 3) & 1)*4)+','+(((x >> 2) & 1)*3)+','+(((x >> 1) & 1)*2)+','+((x & 1)*1)+'/pulse',x & 8,x & 4,x & 2,x & 1)
-			axios.get('http://localhost:3001/API/relay/'+(((x >> 3) & 1)*4)+','+(((x >> 2) & 1)*3)+','+(((x >> 1) & 1)*2)+','+((x & 1)*1)+'/pulse')
-		}, null // function() {console.log('CRON','You will see this message at the end (on stop)');}
-		, true, 'Europe/Paris');
+			// console.log('/API/relay/'+(((x >> 3) & 1)*4)+','+(((x >> 2) & 1)*3)+','+(((x >> 1) & 1)*2)+','+((x & 1)*1)+'/pulse/'+time,x & 8,x & 4,x & 2,x & 1)
+			axios.get('http://localhost:3001/API/relay/'+(((x >> 3) & 1)*4)+','+(((x >> 2) & 1)*3)+','+(((x >> 1) & 1)*2)+','+((x & 1)*1)+'/pulse/'+time)
+		}
+		const job = new CronJob('* * * * * *', () => {callbackCron(time)}, null, true, 'Europe/Paris'); // function() {console.log('CRON','You will see this message at the end (on stop)');}
 		job.start();
+		response.json({cronJob:'enabled'})
+	})
+	.get("/API/cron/stop", (request, response) => {
+		if(job){
+			job.stop();
+			response.json({cronJob:'stoped'})
+		}
 	})
 	.get('/API/relay/', async function (request, response) {
 		let r = []
 		for(let x in relays){
-			relays[x].on();
 			relays[x]._index.query = null
 			relays[x]._index.state = relays[x].state ? 'On':'Off' 
-			relays[x]._index.succes = null
+			relays[x]._index.succes = true
 			r.push(relays[x]._index)
 		}
 		console.log(r)
 		response.json(r)
 	})
-	.get('/API/relay/:relays/:status', async function (request, response) {
+	.get('/API/relay/:relays/:status/:time?', async function (request, response) {
 		//https://raspberrypi.stackexchange.com/questions/66873/how-to-read-output-of-gpio-readall
 
 		if(request.params.relays.match(/(a|A)(l|L){2}/)){request.params.relays = '1,2,3,4'}
@@ -79,10 +92,10 @@ router
 		relays2change = relays.filter(function (element, index, array) {return rlst.includes(element._index.Relay)});
 		// console.log(rlst,relays2change)
 		
-		let relayOn = () => {
+		let relayOn = (t) => {
 			let r = []
 			for(let x in relays2change){
-				relays2change[x].on();
+				relays2change[x].on(t);
 				relays2change[x]._index.query = 'on'
 				relays2change[x]._index.state = relays2change[x].state ? 'On':'Off' 
 				relays2change[x]._index.succes = true
@@ -90,10 +103,10 @@ router
 			}
 			return r
 		}
-		let relayOff = () => {
+		let relayOff = (t) => {
 			let r = []
 			for(let x in relays2change){
-				relays2change[x].off();
+				relays2change[x].off(t);
 				relays2change[x]._index.query = 'off'
 				relays2change[x]._index.state = relays2change[x].state ? 'On':'Off' 
 				relays2change[x]._index.succes = true
@@ -112,12 +125,18 @@ router
 			}
 			return r
 		}
+		let time = NaN
+		if (request.params.time) {time = JSON.parse(request.params.time)}
 		if (request.params.status == 'on') {
-			changed = relayOn()
+			// console.log(time)
+			changed = relayOn(time)
 		} else if(request.params.status == 'pulse') {
-			changed = relayPulse(250)
+			if(!request.params.time){time = 250}
+			// console.log(time)
+			changed = relayPulse(time)
 		} else {
-			changed = relayOff()
+			// console.log(time)
+			changed = relayOff(time)
 		}
 		response.json(changed)
 	})
@@ -154,11 +173,8 @@ router
 		)
 	})
 	.use(function(request, response, next) {
-// $ curl http://rpimaster/notfound
-// $ curl http://rpimaster/notfound -H "Accept: application/json"
-// $ curl http://rpimaster/notfound -H "Accept: text/plain"
 		response.status(404);
-		console.log(request.accepts())
+		console.log('.use(404)',request.accepts())
 		// response.render('404', { url: request.url });
 		response.json({ error: '404 Not found !', url: request.url  });
 		return;
